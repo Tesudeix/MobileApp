cde04d58d47073b0128f9d2f535d77dee7615073
tesudei <tesudeix@gmail.com>
2026-02-12 18:32:28 +0800

    fix metro config + android version config for EAS

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,3 @@
-const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
-const exclusionList = require("metro-config/src/defaults/exclusionList");
 
-const projectRoot = __dirname;
-const backendRoot = path.resolve(projectRoot, "..", "Yuki");
-const escapedBackendRoot = backendRoot.replace(/[\\^$.*+?()[\]{}|]/g, "\\$&");
-
-const config = getDefaultConfig(projectRoot);
-config.resolver.blockList = exclusionList([
-  new RegExp(`${escapedBackendRoot}\\/.*`),
-]);
-config.watchFolders = [projectRoot];
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
